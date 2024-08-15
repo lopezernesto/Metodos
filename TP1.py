5246b8a0231265fe642ce089b177547fd1245703
Ernesto López <ernesto.lopez@est.fi.uncoma.edu.ar>
2024-08-15 02:10:49 -0300

    trapecista y simpson funcionando

--- TP1.py
+++ TP1.py
@@ -1,59 +1,43 @@
 from sympy import symbols
+# Metodo del trapecista
+#a y b son [a,b] de mi integral definida
+def trapecista(a, b, funcion, n):
+    h = (b - a) / n
+    res = (calcular_funcion(funcion, a) + calcular_funcion(funcion, b) +
+           2 * sumatoria(a + h, b - h, h, 1,funcion))
+    res = (h / 2) * res
+    return res
+
+
+def simpson(a, b, funcion, n):
+    h = (b - a) / n
+    res = calcular_funcion(funcion, a) + calcular_funcion(funcion, b)
+    print("pares: ")
+    res += 2 * sumatoria(a + 2 * h, b - h, h, 2,funcion)
+    print("impares")
+    res += 4 * sumatoria(a + h, b - h, h, 2,funcion)
+    res = (h / 3) * res
+    return res
+
+def sumatoria(inicio, fin, h,salto,funcion):
+    suma = 0
+    while (inicio <= fin):
+        print("Estoy en x",inicio)
+        suma += calcular_funcion(funcion, inicio)
+        inicio += h*salto
+    return suma
 
 # funcion con un numero
-
-
 def calcular_funcion(funcion, numero):
     f = funcion.subs(x, numero)
     return f
 
-# Metodo para la funcion de Simpson 1/3
-
-
-def cantidad_equis(x0, xn, h):
-    cont = 0
-    x0 = x0+h
-    while (x0 < xn):
-        x0 += h
-        cont += 1
-    return cont
-
-
-def trapecista(x0, xn, funcion):
-    h = (xn-x0)/4
-    cant = cantidad_equis(x0, xn, h)
-    res = ((calcular_funcion(funcion, x0) +
-            calcular_funcion(funcion, xn))+(2*sumatoria(x0+2*h, cant, h)))
-    res = h/2*res
-    return res
-
-
-def sumatoria(inicio, fin, h):
-    i = 1
-    suma = 0
-    while (i < fin):
-        suma += calcular_funcion(funcion, inicio)
-        print(suma)
-        inicio += h
-        i += 1
-    return suma
-
-
-def sumatoria_impares(inicio, fin, h):
-    i = 1
-    suma = 0
-    while (i < fin):
-        suma += calcular_funcion(funcion, inicio)
-        inicio += 2*h
-        i += 2
-    return 1
-
-
     # Aca empieza el programa
-funcion = "5x^2+2x-3"
 # Definir la variable
 x = symbols('x')
 x0 = 2
 xn = 4
 funcion = 5*x**2+2*x-3
-print(trapecista(x0, xn, funcion))
+print("El resultado de trapecista es:",trapecista(x0, xn, funcion,8))
+print("--------------")
+print("El resultado de simpson es:",simpson(x0, xn, funcion,8))
